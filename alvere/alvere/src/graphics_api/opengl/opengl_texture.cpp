6d264f5101734640654356fb6ad25830c296427d
TheKimeo <jameskime100@googlemail.com>
2019-11-21 10:46:37 +0000

    Revert to old format code to fix runtime nullref.
    This is a tentative change that will hopefully fix nullreferences when drawing on particular PC's

--- alvere/alvere/src/graphics_api/opengl/opengl_texture.cpp
+++ alvere/alvere/src/graphics_api/opengl/opengl_texture.cpp
@@ -63,7 +63,7 @@ namespace alvere::graphics_api::opengl
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		ALV_LOG_OPENGL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_resWidth, m_resHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_pixelData));
+		ALV_LOG_OPENGL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, m_resWidth, m_resHeight, 0, format, GL_UNSIGNED_BYTE, m_pixelData));
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 }
